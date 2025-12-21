12b248c128f4a33a324ee6b66667fc971b98e585
KhaoticKodeDev62 <thecreativeone62@gmail.com>
2025-12-21 12:07:21 -0500

    feat(engine): Implement ManifestParser and Version comparison

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import json
 from concurrent.futures import ThreadPoolExecutor
 
 class VerificationEngine:
@@ -26,3 +27,27 @@ class VerificationEngine:
         
         # Return a dictionary of path -> hash
         return dict(zip(file_paths, results))
+
+class ManifestParser:
+    def __init__(self, json_data):
+        self.data = json.loads(json_data)
+
+    def get_target_version(self):
+        return self.data.get("version")
+
+    def get_patches(self):
+        patch_section = self.data.get("patch", {})
+        return patch_section.get("files", [])
+
+class Version:
+    def __init__(self, version_str):
+        self.parts = [int(x) for x in version_str.split('.')]
+
+    def __lt__(self, other):
+        return self.parts < other.parts
+
+    def __eq__(self, other):
+        return self.parts == other.parts
+
+    def __str__(self):
+        return '.'.join(map(str, self.parts))
